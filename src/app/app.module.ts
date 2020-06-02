4017a5a4bf76d75efac71da87087dcaf821b5e36
Scott Williams <scott.joe.williams@gmail.com>
2020-06-02 16:46:36 -0400

    Makes Form it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HabitDetailComponent } from './habit-detail/habit-detail.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
+import { HabitFormComponent } from './habit-form/habit-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HabitDetailComponent, HabitListComponent],
+  declarations: [AppComponent, HabitDetailComponent, HabitListComponent, HabitFormComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [HabitFormComponent],
 })
 export class AppModule {}
--- src/app/habit-list/habit-list.component.ts
+++ src/app/habit-list/habit-list.component.ts
@@ -5,10 +5,7 @@ import { FormBuilder } from '@angular/forms';
   selector: 'app-habit-list',
   template: `
     <h2>List</h2>
-    <form [formGroup]="habitForm" (ngSubmit)="onSubmit(habitForm.value)">
-      <input type="text" placeholder="Add Habit" formControlName="title" />
-      <button type="submit">Add</button>
-    </form>
+    <app-habit-form (addHabit)="onAddHabit($event)"></app-habit-form>
     <ul>
       <app-habit-detail
         *ngFor="let habit of habits"
@@ -19,7 +16,6 @@ import { FormBuilder } from '@angular/forms';
   styles: [],
 })
 export class HabitListComponent implements OnInit {
-  habitForm: any;
   habits = [
     {
       id: 1,
@@ -43,17 +39,13 @@ export class HabitListComponent implements OnInit {
     },
   ];
 
-  constructor(private formBuilder: FormBuilder) {
-    this.habitForm = this.formBuilder.group({
-      title: '',
-    });
-  }
+  constructor() {}
 
-  onSubmit(newHabit: any) {
-    const id = this.habits.length + 1;
-    newHabit.id = id;
-    this.habits.push(newHabit);
-    this.habitForm.reset();
+  onAddHabit({ title }: any) {
+    this.habits.push({
+      id: this.habits.length + 1,
+      title,
+    });
   }
 
   ngOnInit(): void {}
