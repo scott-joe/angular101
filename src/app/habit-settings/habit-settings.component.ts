ef16eccb0c20cae8de4ddc6404f4a77481a356ca
Scott Williams <scott.joe.williams@gmail.com>
2020-06-04 14:39:41 -0400

    Adds settings component

---
+++ src/app/habit-settings/habit-settings.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-settings',
+  template: `
+    <p>
+      habit-settings works!
+    </p>
+  `,
+  styles: [],
+})
+export class HabitSettingsComponent {
+  constructor() {}
+}
